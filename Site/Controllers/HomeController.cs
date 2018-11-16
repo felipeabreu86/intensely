7f82b1f6ec9482815aad87144f661e96d71fbd60
Felipe Abreu <felipeabreu.rj@gmail.com>
2018-11-16 17:01:56 -0200

    Criação da controller Home e ajuste nas Views relacionadas

---
+++ Site/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Carrega a página principal do site
+        /// </summary>
+        /// <returns>View index</returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
