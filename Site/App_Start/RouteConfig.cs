946da498dac584f3a0e9c668a5648b6c6a7456ba
Felipe Abreu <felipeabreu.rj@gmail.com>
2018-11-16 16:43:17 -0200

    Ajuste das classes do APP_START

--- Site/App_Start/RouteConfig.cs
+++ Site/App_Start/RouteConfig.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace Site
+namespace Site.App_Start
 {
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.MapMvcAttributeRoutes();
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
