23aee0bd60fe901e3a723023633b1b8c5d698ceb
Felipe Abreu <felipeabreu.rj@gmail.com>
2018-11-18 13:26:42 -0200

    Inclusão do layout da área restrita

--- Site/App_Start/BundleConfig.cs
+++ Site/App_Start/BundleConfig.cs
@@ -13,6 +13,21 @@ namespace Site.App_Start
             bundles.Add(new ScriptBundle("~/Content/js").Include(
                 "~/Content/js/*.js"
                 ));
+
+            bundles.Add(new StyleBundle("~/Areas/Restrito/Content/css").Include(
+                "~/Areas/Restrito/Content/css/*.css",
+                "~/Areas/Restrito/Content/css/bootstrap.min.css"
+                ));
+            bundles.Add(new ScriptBundle("~/Areas/Restrito/Content/js").Include(
+                "~/Areas/Restrito/Content/js/*.js",
+                "~/Areas/Restrito/Content/js/core/bootstrap.min.js",
+                "~/Areas/Restrito/Content/js/core/jquery.3.2.1.min.js",
+                "~/Areas/Restrito/Content/js/core/popper.min.js",
+                "~/Areas/Restrito/Content/js/plugins/*.js",
+                "~/Areas/Restrito/Content/js/plugins/chartist.min.js",
+                "~/Areas/Restrito/Content/js/plugins/nouislider.min.js"
+                ));
+
             BundleTable.EnableOptimizations = false;
         }
     }
